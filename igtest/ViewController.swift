7bdbc60e5506f0f285469ab794121126ecdaea54
Abshir <abshirm@live.com>
2019-07-03 19:13:44 -0700

    Initial Commit

---
+++ igtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  igtest
+//
+//  Created by Abshir Mohamed on 7/3/19.
+//  Copyright © 2019 Abshir Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
