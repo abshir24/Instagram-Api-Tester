a90a2001d0b44f26a9d892a9f315c67cb76fef95
Abshir <abshirm@live.com>
2019-07-04 15:38:06 -0700

    successful connection. modularization is needed

--- server/server.js
+++ server/server.js
@@ -14,14 +14,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 require('./routes.js')(app);
 
 ig.use({
-  client_id: ' 76eae290f6df49f88ee48e463f875905',
-  client_secret: '22ad879bc6be411bb063a7875c29dcbb'
+  client_id: '76eae290f6df49f88ee48e463f875905',
+  client_secret: '8389da13245844e2bd57fa5c34d4e41f'
 });
 
 //the redirect uri we set when registering our application
 var redirectUri = 'http://localhost:8000/handleAuth';
 
 app.get('/', function(req, res){
+  console.log("ROOT")
   // create a new instance of the use method which contains the access token gotten
    ig.use({
     access_token : accessToken
@@ -29,17 +30,20 @@ app.get('/', function(req, res){
 
    ig.user_media_recent("access_token.split('.')[0]", function(err, result, pagination, remaining, limit) {
        if(err) res.json(err);
-    // pass the json file gotten to our ejs template
-       res.render('pages/index', { instagram : result });
+        
+       console.log("RESULT", res.json(result))
+  
    });
 });
 
 app.get('/authorize', function(req, res){
+  console.log("AUTHORIZE")
   // set the scope of our application to be able to access likes and public content
   res.redirect(ig.get_authorization_url(redirectUri, { scope : ['public_content','likes']}) );
 });
 
 app.get('/handleAuth', function(req, res){
+  console.log("HANDLE AUTH")
   //retrieves the code that was passed along as a query to the '/handleAuth' route and uses this code to construct an access token
   ig.authorize_user(req.query.code, redirectUri, function(err, result){
       if(err) res.send( err );
